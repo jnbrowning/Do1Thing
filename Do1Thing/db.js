e6fc353a02216e3d10726f85422e23e58dd6602e
Grace Brindle <89871638+gracebrindle@users.noreply.github.com>
2023-02-16 13:51:54 -0500

    Add files to store Firebase information

---
+++ Do1Thing/db.js
@@ -0,0 +1,9 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { firebaseConfig } from "../Secrets";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
