abe702f493c38b8ac537cd42cc50b576f05a2aa2
jnbrowning <jnbrowni@umich.edu>
2023-03-30 17:24:08 -0400

    add ModuleTitle page

---
+++ Do1Thing/components/ModuleTitle.js
@@ -0,0 +1,91 @@
+import { View, StyleSheet, Image } from "react-native";
+import { Text, TextInput, Button } from "@react-native-material/core";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+import { Ionicons } from '@expo/vector-icons'; 
+
+
+function ModuleTitle({navigation, route}) {
+
+  let link = route.params.logo;
+
+  return (
+    <View style={styles.container}>
+      <Button
+      style={styles.backButton}
+      variant='text'
+      title={<Ionicons name="chevron-back-circle-sharp" size={35} color='#1D7DAB' />}
+      onPress={()=>navigation.goBack()}
+      />
+      <View style={styles.bodyContainer}>
+        <Text style={styles.moduleHeading}>Module {route.params.moduleNum}</Text>
+        <Text style={styles.moduleTitle}>{route.params.goal}</Text>
+        <Image style={styles.testIcon} 
+        source={link}/>
+        <Text style={styles.goalHeader}>Goal</Text>
+        <Text style={styles.goalText}>{route.params.moduleName}</Text>
+      </View>
+    </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'flex-start',
+      justifyContent: 'center',
+    },
+    bodyContainer: {
+      flex: 0.8,
+      justifyContent: 'flex-start',
+      alignItems: 'flex-start',
+      // backgroundColor: 'tan',
+      width: '100%',
+    },
+    heading: {
+      fontSize: 24,
+      color: 'black',
+      paddingBottom: '5%',
+      paddingTop: '5%',
+      fontWeight: 'bold'
+    },
+    testIcon: {
+      height: 200,
+      width: 200,
+      marginTop: 75,
+      marginBottom: 75,
+      alignSelf: 'center',
+      color: 'blue',
+    },
+    backButton: {
+      height: 70,
+      paddingLeft: '5%',
+      justifyContent: 'center'
+    },
+    moduleHeading: {
+      color: '#12B1C3',
+      fontSize: 20,
+      paddingLeft: '10%',
+    },
+    moduleTitle: {
+      color: '#0E5681',
+      fontSize: 36,
+      paddingTop: 10,
+      paddingBottom: 10,
+      fontWeight: 'bold',
+      paddingLeft: '10%',
+    },
+    goalHeader: {
+      paddingLeft: '10%',
+      color: "#1D7DAB",
+      fontWeight: 'bold',
+      fontSize: '32',
+    },
+    goalText: {
+      paddingLeft: '10%',
+      paddingTop: '3%',
+      fontSize: 20,
+    },
+  });
+
+  export default ModuleTitle;
