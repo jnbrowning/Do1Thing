a38308241889235cc2a6794cfe3e7a197062faac
QW <wangqy@umich.edu>
2023-04-10 16:43:01 -0600

    Configure app for build

---
+++ Do1Thing/data/Secret.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyCMyqWlFFXxK2JOmjtT1ef_00_6Wau0l3U',
+    authDomain: 'do1thing-3be9d.firebaseapp.com',
+    projectId: 'do1thing-3be9d',
+    storageBucket: 'do1thing-3be9d.appspot.com',
+    messagingSenderId: '580201853625',
+    appId: '1:580201853625:web:7b683367e6de0427cbe168',
+    measurementId: 'G-6T1BG2NV4H'
+  };
+  export {firebaseConfig};
